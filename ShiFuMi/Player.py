8cf7dbdb1033db7de2ba689484ce6bc0748a776e
Hélène DC <h.decock92@gmail.com>
2019-05-29 13:16:56 +0200

    Computer and two player version, win message. Delete choice class

--- ShiFuMi/Player.py
+++ ShiFuMi/Player.py
@@ -6,6 +6,30 @@ class PlayerType:
         self.name = name
         self.win = 0
         self.attack =""
+        self.choice = ["rock", "paper", "scissors"]
+    
+    def fight (self, player):
+        if self.attack == player.attack:
+            print("Égalité, rejoué")
+        elif ((self.attack == "rock" or player.attack == "rock") and (self.attack == "paper" or player.attack == "paper")):
+            print("Paper Win!")
+            if(self.attack == "paper"):
+                self.win += 1
+            else:
+                player.win += 1
+        elif ((self.attack == "scissors" or player.attack == "scissors") and (self.attack == "paper" or player.attack == "paper")):
+            print("Scissors Win!")
+            if(self.attack == "scissors"):
+                self.win += 1
+            else:
+                player.win += 1
+        else:
+            print("Rock win")
+            if(self.attack == "rock"):
+                self.win += 1
+            else:
+                player.win += 1
+
 
 class Computer(PlayerType):
     def __init__(self):
@@ -13,15 +37,10 @@ class Computer(PlayerType):
         super().__init__(self.name)
     
     def ChoiceAttack(self):
-        number = randint(0,3)
-
-        if number == 1:
-            self.attack = "Paper"
-        elif number == 2:
-            self.attack = "Scissors"
-        elif number == 3:
-            self.attack = "Rock"
+        number = randint(0,2)
         
+        self.attack = self.choice[number]
+
         return self.attack
 
 
--- ShiFuMi/main_pierrepapier.py
+++ ShiFuMi/main_pierrepapier.py
@@ -1,19 +1,40 @@
-from Choice_possibility import *
 from Player import *
 
-player = Player(input("Entrez un nom de joueur : "))
-computer = Computer()
 
-computer = computer.ChoiceAttack()
-player.attack = input("entrez un choix")
+number = int(input("How many players are you ? (1 or 2) : "))
 
-computer.fight(player)
+if (number%2 != 0):
+    player = Computer()
+    player1 = Player(input("Entrez un nom de joueur : "))
+else:
+    player = Player(input("Entrez un nom de joueur : "))
+    player1 = Player(input("Entrez un nom de joueur : "))
 
-choice = Rock()
-choice2 = Scissor()
-choice3 = Paper()
-choice.fight(choice3)
-choice.fight(choice2)
-choice2.fight(choice)
-choice.fight(choice)
+
+while (player.win < 3 and player1.win < 3):
+    if (number == 1):
+        player.ChoiceAttack()
+        print(player.attack)
+    else:
+        player.attack = input("Choisissez une attaque : ")
+    player1.attack = input("Choisissez une attaque : ")
+
+    print(player.win)
+    print(player1.win)
+
+    player1.fight(player)
+
+
+    print(player.win)
+    print(player1.win)
+
+if (player.win ==3):
+    print(player.name, " , win !")
+else:
+    print(player1.name, " , win !")
+
+
+
+
+
 
