166d14c4aae1fa9c4d260642bf35c08afe91c19f
Hélène DC <h.decock92@gmail.com>
2019-05-29 14:57:07 +0200

    add restart

--- ShiFuMi/main_pierrepapier.py
+++ ShiFuMi/main_pierrepapier.py
@@ -2,36 +2,43 @@ from Player import *
 
 
 number = int(input("How many players are you ? (1 or 2) : "))
+restart = True
+answer = ""
 
 if (number%2 != 0):
     player = Computer()
-    player1 = Player(input("Entrez un nom de joueur : "))
+    player1 = Player(input("Enter a player name : "))
 else:
-    player = Player(input("Entrez un nom de joueur : "))
-    player1 = Player(input("Entrez un nom de joueur : "))
+    player = Player(input("Enter a player name : "))
+    player1 = Player(input("Enter a player name : "))
 
 
-while (player.win < 3 and player1.win < 3):
-    if (number == 1):
-        player.ChoiceAttack()
-        print(player.attack)
+while restart:
+
+    while (player.win < 3 and player1.win < 3):
+
+        if (number == 1):
+            player.ChoiceAttack()
+            print(player.attack)
+        else:
+            player.attack = input("Choose your attack : ")
+        player1.attack = input("Choose your attack : ")
+
+        player1.fight(player)
+
+
+        print(player.name, "has: ", player.win, " points !")
+        print(player1.name, "has: ", player1.win, " points !")
+
+    if (player.win == 3):
+        print(player.name, ", win !")
     else:
-        player.attack = input("Choisissez une attaque : ")
-    player1.attack = input("Choisissez une attaque : ")
+        print(player1.name, ", win !")
 
-    print(player.win)
-    print(player1.win)
+    answer = input("Do you want to restart ? (yes or no)")
 
-    player1.fight(player)
-
-
-    print(player.win)
-    print(player1.win)
-
-if (player.win ==3):
-    print(player.name, " , win !")
-else:
-    print(player1.name, " , win !")
+    if answer == "no":
+        restart = False
 
 
 
